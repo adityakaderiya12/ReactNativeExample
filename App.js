b2727d17512576f6250f02bb87bd2d10dfcba3bc
adityakaderiya12 <adityakaderiya12@gmail.com>
2022-11-01 23:02:11 +0545

    AB-Add component TextInpur

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput value ="Aditya kaderiya" />
       <Text>Welcome TO Agrim Booking Project</Text>
       <StatusBar style="auto" />
     </View>
